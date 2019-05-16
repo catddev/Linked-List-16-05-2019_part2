3acd85efe52c1d3f26fd93c5a82adcc4737c3545
айкеновад <@205_8>
2019-05-16 12:00:02 +0600

    Добавьте файлы проекта.

---
+++ class LinkedList_16.05.2019_part2/List.h
@@ -0,0 +1,73 @@
+﻿#pragma once
+#include<iostream>
+#include<string>
+using namespace std;
+
+template<typename T>
+class List {
+private:
+	//внтуренний класс
+	class Node {//узелок
+		T el;
+		Node* nextPtr;
+	public:
+		Node(T el, Node*nextPtr) {
+			this->el = el;
+			this->nextPtr = nextPtr;
+		}
+		T& getEl() {return el;}
+		Node*& getNextPtr() {return nextPtr;}
+
+		void setEl(T el) {this->el = el;}
+		void setNextPtr(T* ptr) {this->nextPtr = ptr;}
+
+	};
+
+	Node *firstPtr, *lastPtr;//!!! *
+	int size;
+
+public:
+	List();
+	int getSize();
+	void push_back(T el);
+	void dump();
+
+};
+
+template<typename T>
+inline List<T>::List()
+{
+	firstPtr = lastPtr = NULL;
+	size = 0;
+}
+
+template<typename T>
+inline int List<T>::getSize()
+{
+	return size;
+}
+
+template<typename T>
+inline void List<T>::push_back(T el)
+{
+	if (size == 0) {
+		Node* ptr = new Node(el, NULL);//выделяем место для нового элемента, у которого оба указателя указывают на один и тот же единственный элемент:
+		firstPtr = lastPtr = ptr;
+	}
+	else {
+		Node* ptr = new Node(el, NULL);
+		lastPtr->getNextPtr() = ptr;//выделяем место в памяти и связываем новый элемент с предыдущим??
+		lastPtr = ptr;//переназначение указателя
+	}
+	size++;
+}
+
+template<typename T>
+inline void List<T>::dump()
+{
+	Node*ptr = firstPtr;
+	while (ptr != lastPtr->getNextPtr()) {
+		cout << ptr->getEl() << endl;
+		ptr = ptr->getNextPtr();
+	}
+}
---
+++ class LinkedList_16.05.2019_part2/Source.cpp
@@ -0,0 +1,13 @@
+﻿#include"List.h"
+
+int main() {
+	List<int> l;
+	l.push_back(5);
+	l.push_back(6);
+	l.push_back(7);
+	l.dump();
+
+
+	system("pause");
+	return 0;
+}
