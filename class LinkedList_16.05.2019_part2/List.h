3257a6ea9eb165153dfae41560ceba20530e2693
айкеновад <@205_8>
2019-05-21 12:30:32 +0600

    -

--- class LinkedList_16.05.2019_part2/List.h
+++ class LinkedList_16.05.2019_part2/List.h
@@ -1,7 +1,26 @@
 ﻿#pragma once
 #include<iostream>
 #include<string>
+#include<exception>//!!!
 using namespace std;
+
+//для улавливания ошибок
+class my_exception : public exception {
+public:
+	virtual const char* what() const throw()
+	{
+		return "out of range\n";
+	}
+	int ErrorCode() {
+		return 404;
+	}
+}my_ex;
+
+//готовый класс
+out_of_range lower("out of lower bound");
+out_of_range upper("out of upper bound");
+
+//exception e; //then use "throw e"
 
 template<typename T>
 class List {
@@ -28,8 +47,16 @@ private:
 
 public:
 	List();
+	List(const List&obj);
+	~List();
 	int getSize();
+	T& operator[](int index);
 	void push_back(T el);
+	void push_front(T el);
+	void insert_by_pos(T el, int pos);
+	void pop_front();
+	void pop_back();
+	//void del_by_pos(T el, int pos);
 	void dump();
 
 };
@@ -42,9 +69,63 @@ inline List<T>::List()
 }
 
 template<typename T>
+inline List<T>::List(const List & obj)
+{
+	Node*cur_ptr = obj.firstPtr;
+
+	while (cur_ptr != obj.lastPtr->getNextPtr()) {
+		push_back(cur_ptr->getEl());
+		cur_ptr = cur_ptr->getNextPtr();
+	}
+}
+
+template<typename T>
+inline List<T>::~List()
+{
+	int general_size = size;//tmp size because size is changing every time
+	for (int i = 0; i < general_size; i++)
+		pop_front();
+	firstPtr = lastPtr = 0;
+}
+
+template<typename T>
 inline int List<T>::getSize()
 {
 	return size;
+}
+
+template<typename T>
+inline T& List<T>::operator[](int index)throw()//значит собираемся обработать ошибку
+{
+	int counter = 0;
+	Node*cur_ptr = firstPtr;
+	//если индекс выходит за пределы списка
+	if (index >= size) {//обработка исключений
+		//throw my_ex;//обработай ошибку типа my_ex
+		throw upper;
+	}
+	else if (index < 0)
+		throw lower;
+	else {
+		//var 1
+		//while (cur_ptr != lastPtr) {//проверка до ЛастПтр
+
+		//	if (counter == index)
+		//		return cur_ptr->getEl();
+		//	cur_ptr = cur_ptr->getNextPtr();
+		//	counter++;
+		//}
+		//if (counter == index) return cur_ptr->getEl();//проверка на ЛастПтр
+
+		//var2
+		while (cur_ptr != lastPtr->getNextPtr()) {//здесь сразу включая самый последний элемент
+
+			if (counter == index)
+				return cur_ptr->getEl();
+			cur_ptr = cur_ptr->getNextPtr();
+			counter++;
+		}
+	}
 }
 
 template<typename T>
@@ -63,11 +144,77 @@ inline void List<T>::push_back(T el)
 }
 
 template<typename T>
+inline void List<T>::push_front(T el)
+{
+	Node *cur_ptr = new Node(el, firstPtr);//при создании сразу задаем что некстПтр нового каррент птр = старый ферстПтр
+	firstPtr = cur_ptr;
+	size++;
+	if (size == 1)//т.е. список был пустой и после добавления всего один элемент содержит
+			lastPtr = firstPtr;
+}
+
+template<typename T>
+inline void List<T>::insert_by_pos(T el, int pos)
+{
+	
+	Node *cur_ptr = firstPtr;
+	
+	int counter = 0;
+
+	if (pos >= 0 && pos < size) {
+		while (counter != pos - 1) {//до предыдущего
+			cur_ptr = cur_ptr->getNextPtr();
+			counter++;
+		}
+		cur_ptr = cur_ptr->getNextPtr();
+		Node insert(el, cur_ptr);
+		insert.getNextPtr() = cur_ptr->getNextPtr();
+		cur_ptr = insert.getNextPtr();
+		
+		size++;
+	}
+	else
+		cout << "Unacceptable position" << endl;
+}
+
+template<typename T>
+inline void List<T>::pop_front()
+{
+	if (size != 0)//list is not empty
+	{
+		Node* cur_ptr = firstPtr;
+		firstPtr = firstPtr->getNextPtr();
+		delete cur_ptr;
+		size--;
+	}
+	else
+		cout << "Trying to delete from empty list" << endl;
+}
+
+template<typename T>
+inline void List<T>::pop_back()
+{
+	if (size != 0) {
+		Node*cur_ptr = firstPtr;
+		Node*tmp_lastPtr = lastPtr;//сохраняем ластПтр
+		while (cur_ptr->getNextPtr() != lastPtr) {//идем до последнего и когда доходим до него, то в цикл больше не войдем, т.е. остановимся на ПРЕДпоследнем
+			cur_ptr = cur_ptr->getNextPtr();//переставляем на следующий
+		}
+		lastPtr = cur_ptr;//переставляем ластПтр на ПРЕДпоследний
+		delete tmp_lastPtr;//удаляем сам старый ЛастПтр
+		size--;
+	}
+}
+
+template<typename T>
 inline void List<T>::dump()
 {
-	Node*ptr = firstPtr;
-	while (ptr != lastPtr->getNextPtr()) {
-		cout << ptr->getEl() << endl;
-		ptr = ptr->getNextPtr();
+	if (size != 0)
+	{
+		Node*ptr = firstPtr;
+		while (ptr != lastPtr->getNextPtr()) {
+			cout << ptr->getEl() << endl;
+			ptr = ptr->getNextPtr();
+		}
 	}
 }
--- class LinkedList_16.05.2019_part2/Source.cpp
+++ class LinkedList_16.05.2019_part2/Source.cpp
@@ -2,10 +2,41 @@
 
 int main() {
 	List<int> l;
-	l.push_back(5);
-	l.push_back(6);
-	l.push_back(7);
-	l.dump();
+	//рассматриваем блок, где может возникнуть ошибка
+	try {
+		l.push_back(5);
+		l.push_back(6);
+		l.push_back(7);
+		l.push_front(200);
+		l.push_front(500);
+		l.insert_by_pos(777, 3);
+		//l.pop_front();
+		//l.pop_back();
+
+		l.dump();
+		cout << endl;
+		for (int i = l.getSize(); i > 0; i--)
+			cout << l[i - 1] << endl;
+
+		cout << endl << l.operator[](0) << endl;//the same as l[0]
+		//cout << l[100] << endl;//после первой же ошибки программа прерывается
+		cout << l[-3] << endl;
+	}
+	//catch (my_exception my_ex) {
+	//	//cout << my_ex.what() << endl;
+	//	cout << my_ex.ErrorCode() << endl;
+	//}
+	catch (out_of_range my_ex) {
+		cout << my_ex.what() << endl;
+	}
+
+	List<int> l2(l);
+	cout << "COPY" << endl;
+	l2.dump();
+
+	l.~List();//явно деструктор вызываем
+
+
 
 
 	system("pause");
